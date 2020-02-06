c10449a8424e32a6b86fbb2805d29227fcc4f0c3
Connor Bach <ConnorBach@users.noreply.github.com>
2020-02-06 15:44:52 -0500

    Grid (#3)
    * initial setup
    
    * added working grid demo
    
    * fixing merge conflicts
    
    * I added a new component to be able to remove widgets from the board
    
    Co-authored-by: Timothy Blazek <35946299+tblazek98@users.noreply.github.com>

--- pisign-frontend/src/App.vue
+++ pisign-frontend/src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Grid :layout="layout"></Grid>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Grid from './components/Grid.vue'
+
+var testLayout = [
+  {"x":0,"y":0,"w":2,"h":2,"i":"0"},
+  {"x":2,"y":0,"w":2,"h":4,"i":"1"},
+  {"x":4,"y":0,"w":2,"h":5,"i":"2"},
+  {"x":6,"y":0,"w":2,"h":3,"i":"3"},
+  {"x":8,"y":0,"w":2,"h":3,"i":"4"},
+  {"x":10,"y":0,"w":2,"h":3,"i":"5"},
+  {"x":0,"y":5,"w":2,"h":5,"i":"6"},
+  {"x":2,"y":5,"w":2,"h":5,"i":"7"},
+  {"x":4,"y":5,"w":2,"h":5,"i":"8"},
+  {"x":6,"y":3,"w":2,"h":4,"i":"9"},
+  {"x":8,"y":4,"w":2,"h":4,"i":"10"},
+  {"x":10,"y":4,"w":2,"h":4,"i":"11"},
+  {"x":0,"y":10,"w":2,"h":5,"i":"12"},
+  {"x":2,"y":10,"w":2,"h":5,"i":"13"},
+  {"x":4,"y":8,"w":2,"h":4,"i":"14"},
+  {"x":6,"y":8,"w":2,"h":4,"i":"15"},
+  {"x":8,"y":10,"w":2,"h":5,"i":"16"},
+  {"x":10,"y":4,"w":2,"h":2,"i":"17"},
+  {"x":0,"y":9,"w":2,"h":3,"i":"18"},
+  {"x":2,"y":6,"w":2,"h":2,"i":"19"}
+	];
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Grid
+  },
+  data: function() {
+    return {
+      layout: testLayout
+    }
   }
 }
 </script>
---
+++ pisign-frontend/src/components/Grid.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+    <h1>Grid</h1>
+    <GridLayout
+        :layout.sync="layout"
+        :col-num="12"
+        :row-height="30"
+        :is-draggable="true"
+        :is-resizable="true"
+        :is-mirrored="false"
+        :vertical-compact="true"
+        :margin="[10, 10]"
+        :use-css-transforms="true"
+        >
+    <GridItem v-for="(item, index) in layout"
+                   :x="item.x"
+                   :y="item.y"
+                   :w="item.w"
+                   :h="item.h"
+                   :i="item.i"
+                   :key="item.i"
+                   style="background: lightblue;"
+                   >
+            {{item.i}}
+            <CloseButton :index="index" :layout="layout"></CloseButton>
+    </GridItem>
+    </GridLayout>
+    </div>
+</template>
+
+<script>
+import VueGridLayout from 'vue-grid-layout';
+import CloseButton from './CloseButton.vue'
+export default {
+  name: 'Grid',
+  props: [ 'layout' ],
+  components: {
+      GridLayout: VueGridLayout.GridLayout,
+      GridItem: VueGridLayout.GridItem,
+      CloseButton: CloseButton
+  }
+}
+</script>
