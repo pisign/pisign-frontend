fa19adda3ff18446edd855b270bab2415b4ab1b8
Timothy Blazek <tblazek@nd.edu>
2020-02-12 17:30:07 -0500

    Made naming of files consistent

--- pisign-frontend/src/components/widget_settings/weather.vue
+++ pisign-frontend/src/components/widget_settings/weather.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-form>
         <v-text-field v-model="apiKey" label="API Key*" required></v-text-field>
-        <v-text-field v-model="zip" label="ZIP Code*" required></v-text-field>
+        <v-text-field @keypress="isZip($event)" v-model="zip" label="ZIP Code*" required></v-text-field>
       </v-form>
     </v-row>
   </v-container>
@@ -24,9 +24,21 @@ export default {
     }
   },methods:{
     sendInfo(){
-      this.zip = parseInt(this.zip);
-      this.$emit('FormFilling', JSON.parse(JSON.stringify(this.$data)))
+      var send_JSON = JSON.parse(JSON.stringify(this.$data));
+      send_JSON.zip = parseInt(this.zip);
+      this.$emit('FormFilling', send_JSON);
+    }, isZip(evt){
+      evt = (evt) ? evt : window.event;
+      var charCode = (evt.which) ? evt.which : evt.keyCode;
+      if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+        evt.preventDefault();
+      } else if (this.zip > 9999){
+        evt.preventDefault();
+      } else {
+        return true;
+      }
     }
+
   }
 }
 </script>
