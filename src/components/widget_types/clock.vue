1bbf2cb9b287bd9f58c16e6c59dad63fffeedc78
Timothy Blazek <tblazek@nd.edu>
2020-02-20 16:29:41 -0500

    Fixed api data parsing and changed the layout for clock a little

--- src/components/widget_types/clock.vue
+++ src/components/widget_types/clock.vue
@@ -2,10 +2,20 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12" class="pa-0">
-        <p class="mb-0">{{ time }}</p>
+        <p>{{ city }}</p>
+      </v-col>
+    </v-row>
+    <v-row v-if='timestamp != ""' class="text-center">
+      <v-col cols="12" class="pa-0">
+        <p class="mb-0">{{ timestamp | moment('timezone', timeZone , "h:mm:ss a")}}</p>
       </v-col>
       <v-col cols="12" class="pa-0">
-        <p class="mb-0">{{ date }}</p>
+        <p class="mb-0">{{ timestamp | moment('timezone', timeZone , "MMMM D YYYY") }}</p>
+      </v-col>
+    </v-row>
+    <v-row v-else class="text-center">
+      <v-col cols="12" class="pa-0">
+        <p class="mb-0">Failed to fetch time.</p>
       </v-col>
     </v-row>
   </v-container>
@@ -21,8 +31,7 @@ export default {
   name: 'ClockWidget',
   data: function(){
     return {
-      time: "",
-      date: "",
+      timestamp : "",
       timeZone: this.api.Location
     }
   }, props:{
@@ -31,20 +40,26 @@ export default {
     }, api : {
       required: true
     }
+  }, computed: {
+    city : function(){
+      try {
+        return this.timeZone.split("/")[1].replace(/_/g, " ");
+      } catch {
+        return this.timeZone;
+      }
+    }
   },
   watch : {
     // Run function on change of the sent data API
     sentData : function(){
       // If the API was successful
       if (this.sentData.Status == "success"){
-        var date_obj = new Date(Date.parse(this.sentData.Time));
-        this.time = date_obj.toLocaleTimeString('en-US', {timeZone: this.timeZone});
-        this.date = date_obj.toLocaleDateString('en-US', {timeZone: this.timeZone});
+        var data = this.sentData.Data;
+        this.timestamp = data.Time > 0 ? data.Time : "";
       }
        // If the API sent failed in some way
       else if (this.sentData.Status == "failure") {
-        this.time = "N/A";
-        this.date = "N/A";
+        this.timestamp = "";
       }
     }, api : function() {
       // If they change the clock Location
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 // Importing vuetify
 import vuetify from './plugins/vuetify.js' // path to vuetify export
+import VueMoment from 'vue-moment'
+import moment from 'moment-timezone'
+
+Vue.use(VueMoment, {
+    moment,
+})
 
 Vue.config.productionTip = false
 
