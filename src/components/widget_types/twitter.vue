a2134790d9aac17aeaf87695707faa1754806f4e
Connor Bach <cbach120@gmail.com>
2020-03-02 14:15:20 -0500

    working on twitter widget

--- src/components/widget_types/twitter.vue
+++ src/components/widget_types/twitter.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <v-card v-for="tweet in tweets" v-bind:key="tweet.text">
+        <v-card v-for="tweet in tweets.slice(0, numberTweets)" v-bind:key="tweet.text">
             <v-card-title>
                 <v-icon
-                    large
                     left
                 >
                     mdi-twitter
@@ -11,7 +10,7 @@
                 <span class="title font-weight-light">Twitter</span>
             </v-card-title>
 
-            <v-card-text class="headline font-weight-bold">
+            <v-card-text class="font-weight-bold">
                 {{tweet.text}}
             </v-card-text>
 
@@ -20,12 +19,12 @@
                     <v-list-item-avatar color="grey darken-3">
                     <v-img
                         class="elevation-6"
-                        src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+                        src="https://static.nd.edu/images/webclips/default/webclip-1024.png"
                     ></v-img>
                     </v-list-item-avatar>
 
                     <v-list-item-content>
-                    <v-list-item-title>{{tweet.user}}</v-list-item-title>
+                    <v-list-item-title>{{userId}}</v-list-item-title>
                     </v-list-item-content>
 
                     <v-row
@@ -33,10 +32,10 @@
                     justify="end"
                     >
                     <v-icon class="mr-1">mdi-heart</v-icon>
-                    <span class="subheading mr-2">256</span>
+                    <span class="subheading mr-2">{{tweet.likes}}</span>
                     <span class="mr-1">·</span>
                     <v-icon class="mr-1">mdi-share-variant</v-icon>
-                    <span class="subheading">45</span>
+                    <span class="subheading">{{tweet.shares}}</span>
                     </v-row>
                 </v-list-item>
             </v-card-actions>
@@ -55,8 +54,27 @@
         name: 'TwitterWidget',
         data: function() {
             return {
-                tweets: [{user: "Notre Dame CSE", text: "Notre Dame Seniors build amazing open source projects!"}, {user: "ND CSE", text: "News: ND opening new computer lab"}],
-                user: ''
+                tweets: [
+                    {user: "ND", text: "Notre Dame seniors build amazing open source projects!", likes: 256, shares: 24},
+                    {user: "ND", text: "News: ND opening new computer lab", likes: 311, shares: 42}
+                ],
+                numberTweets: this.api.NumberTweets,
+                apiKey: this.api.APIKey,
+                userId: this.api.UserId
+            }
+        },
+        props: {
+            sentData: Object,
+            api: Object,
+        },
+        watch: {
+            sentData: function() {
+
+            },
+            api: function () {
+                this.numberTweets = this.api.NumberTweets
+                this.apiKey = this.api.APIKey
+                this.userId = this.api.UserId
             }
         }
     }
